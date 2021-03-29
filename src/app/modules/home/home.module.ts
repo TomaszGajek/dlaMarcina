37a42b75c76e3ef28daf45a4b3f822db46810534
Tomek <kosmagajek@gmail.com>
2021-03-29 15:31:18 +0200

    chore: modyfing husky hooks

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { HomeComponent } from './pages/home/home.component';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from '@shared/shared.module';
+import { HomeComponent } from './pages/home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { MapComponent } from './components/map/map.component';
 import { ListComponent } from './components/list/list.component';
-import { SharedModule } from '@shared/shared.module';
-import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [HomeComponent, MapComponent, ListComponent],
